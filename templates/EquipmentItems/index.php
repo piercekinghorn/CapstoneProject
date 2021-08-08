97f2f687c029fcc7b2fbdc97e23049687240309c
JackRolfe <jack.rolfe@cqumail.com>
2021-08-08 11:54:51 +1000

    deployment test

--- templates/EquipmentItems/index.php
+++ templates/EquipmentItems/index.php
@@ -6,7 +6,7 @@
 ?>
 <div class="equipmentItems index content">
     <?= $this->Html->link(__('New Equipment Item'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Equipment IItems') ?></h3>
+    <h3><?= __('Equipment Items Test') ?></h3>
     <div class="table-responsive">
         <table>
             <thead>
