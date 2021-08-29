e29246abb87ef17f917cb73c37b5e46eaf4ee68a
mrenaudcqu <mylesr@gmail.com>
2021-08-29 18:53:18 +1000

    TR need work v0.5

--- templates/EquipmentItems/index.php
+++ templates/EquipmentItems/index.php
@@ -50,7 +50,7 @@ $this->Html->scriptEnd()
     <div class="table-responsive">
         <table>
             <!--<thead>-->
-                <tr class = "card1">
+                <tr class = "tr1">
                     <th><?= $this->Paginator->sort('equipment_name', 'Equipment') ?></th>
                     <th><?= $this->Paginator->sort('equipment_campus', 'Campus') ?></th>
                     <th><?= $this->Paginator->sort('equipment_lab', 'Lab') ?></th>
@@ -61,15 +61,15 @@ $this->Html->scriptEnd()
             <tbody>
                 <?php foreach ($equipmentItems as $equipmentItems): ?>
                     <?php if ($equipmentItems->equipment_status == '1'): ?>
-                        <tr>
-                            <td><?= h($equipmentItems->equipment_name) ?></td>
+                        <tr class = "tr2">
+                            <td class = "td1"><?= $this->Html->link(__(h($equipmentItems->equipment_name)), ['action' => 'view', $equipmentItems->equipment_id]) ?></td> <!-- Turns all items to links to 'View' -->
                             <td><?= h($equipmentItems->equipment_campus) ?></td>
                             <td><?= h($equipmentItems->equipment_lab) ?></td>
                             <td><?= h($equipmentItems->equipment_details) ?></td>       
                             <td class="actions">
                                 <?= $this->Html->link(__('View'), ['action' => 'view', $equipmentItems->equipment_id]) ?>
                                 <?= $this->Html->link(__('Edit'), ['action' => 'edit', $equipmentItems->equipment_id]) ?>                        
-                                <?= $this->Form->postLink(__('Book'), ['action' => 'book', $equipmentItems->equipment_id]) ?> <!-- Lab Booking Button -->
+                                <?= $this->Form->postLink(__('Book'), ['action' => 'book', $equipmentItems->equipment_id]) ?>
                                 <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $equipmentItems->equipment_id], ['confirm' => __('Are you sure you want to delete # {0}?', $equipmentItems->equipment_id)]) ?>
                             </td>
                         </tr>
