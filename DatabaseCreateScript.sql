93f9983ee6425de21bd0f283c5479b68457622c7
RJGY <54159414+RJGY@users.noreply.github.com>
2021-09-07 13:01:14 +1000

    Added second perm levels. (#138)

--- DatabaseCreateScript.sql
+++ DatabaseCreateScript.sql
@@ -122,8 +122,12 @@ create table users
   user_id int(10) unsigned not null auto_increment primary key,
   username varchar(20) not null,
   password varchar(255),
-  is_staff boolean default false not null
+  student_id int(20) unsigned,
+  is_staff boolean default false not null,
+  is_admin boolean default false not null
 );
 
 insert into users values 
-  (NULL, 'jack', 'test', false);
+  (NULL, 12109625, 'jason', 'password', true, false),
+  (NULL, 12109626, 'jack', 'test', false, false),
+  (NULL, 12109627, 'reese', 'password', true, true);
