d5be088a4088dd64aa988345508765ab3e5ecd09
PierceKH <87044056+AllPurple@users.noreply.github.com>
2021-09-27 06:48:35 +1000

    seed fix

--- DatabaseCreateScript.sql
+++ DatabaseCreateScript.sql
@@ -60,43 +60,58 @@ create table lab_bookings
   return_date TIMESTAMP not null,
   booking_status boolean default false not null
 );
+create table equipment_items
+(
+  equipment_id SERIAL primary key,
+  equipment_name varchar not null,
+  equipment_campus varchar not null,
+  equipment_lab varchar not null,
+  equipment_location varchar not null,
+  equipment_discipline varchar null,
+  equipment_details varchar null,
+  equipment_media varchar null, 
+  equipment_whs varchar null,
+  equipment_status int not null
+);
 
-insert into equipment_items values
-  (1, 'Automatic Level', 'Cairns', 'ROK 28/LG.01', 'Cab1-S4', 'Civil', 'leveling', null, null, 1),
-  (2, 'Automatic Level tripod', 'Cairns', 'ROK 28/LG.01', 'Cab1-S2', 'Civil', 'leveling', null, null, 1),
-  (3, 'Leica Total Station (S06 plus)', 'Cairns', 'ROK 28/LG.04', 'Cab1-S3', 'Civil', 'surveying, measurements', null, null, 1),
-  (4, 'Leica Total Station (S06)', 'Cairns', 'ROK 28/LG.04', 'Cab1-S4', 'Civil', 'surveying, measurements', null, null, 1),
-  (5, 'leica total Station tripod', 'Cairns', 'ROK 28/LG.04', 'Cab2-S1', 'Civil', 'surveying, measurements', null, null, 1),
-  (6, 'GNSS Leica Receiver (GS14)', 'Cairns', 'ROK 28/LG.04', 'Cab2-S3', 'Civil', 'communications', null, null, 1),
-  (7, '2 Tonne Bridge Crane', 'Cairns', 'ROK 28/LG.01', 'Cab3-S6', 'Civil', 'lifting', null, null, 1),
-  (8, 'Flash Forge Creator 3 (FDM)', 'Melbourne', '6.25','Cab3-S3', 'Multi Purpose', '3D printing', null, 'https://www.flashforge.com/product-detail/1', 1),
-  (9, 'Funmat HT 3D Printer (FDM)', 'Melbourne', '6.25','Cab1-S4', 'Multi Purpose', '3D printing', null, 'https://www.intamsys.com/funmat-ht-3d-printer/', 1),
-  (10, 'Formbot Trex 3 (FDM)', 'Melbourne', '6.25','Cab4-S3', 'Multi Purpose', '3D printing', null, 'https://www.formbot3d.com/vivedino-t-rex-30-multi-function-big-size-3d-printer-p0062.html', 1),
-  (11, 'Formlabs 2 (SLA)', 'Melbourne', '6.25','Cab1-S2', 'Multi Purpose', 'measurements', null, 'https://formlabs.com/3d-printers/form-2/', 1),
-  (12, 'Tenma Programmable Power Supply 0-30V 10Amps', 'Melbourne', '6.25','Cab2-S4', '','remote control, power', null,'http://www.farnell.com/datasheets/2805374.pdf', 1),
-  (13, 'FESTO / LabVolt 8006 Power & Machines Trainers (qty 3)', 'Melbourne', '6.25','Cab3-S5', 'Electrical', 'power', null,'https://www.labvolt.com/solutions/6_electricity_and_new_energy/59-8006-10_computer_assisted_0_2_kw_electromechanical_training_system', 1),
-  (14, 'Oscilloscopes', 'Rockhampton', 'ROK 28/2.13','Cab1-S2', 'Multi Purpose', 'GUI, electrical signal', null, 'Standard lab induction', 1),
-  (15, 'Oscillators', 'Rockhampton', 'ROK 28/2.13','Cab2-S1', 'Multi Purpose', 'power, current conversion', null, 'Standard lab induction', 1),
-  (16, 'Multimeters', 'Rockhampton', 'ROK 28/2.13','Cab5-S4', 'Multi Purpose', 'power, measurements', null, 'Standard lab induction', 1),
-  (17, 'Power Supplies', 'Rockhampton', 'ROK 28/2.13','Cab6-S3', 'Multi Purpose', 'power, conversion', null, 'Standard lab induction', 1),
-  (18, 'Sinterit Lisa Pro 3D Printer', 'Rockhampton','ROK 28/2.16', 'Cab1-S4', 'Multi Purpose', '3D printing', null, 'Standard lab induction', 1),
-  (19, 'Funmat HT 3D Printer', 'Rockhampton', 'ROK 28/2.16','Cab2-S2', 'Multi Purpose', 'FFF, 3D printing', null, 'Standard lab induction', 1),
-  (20, 'Metal 3D Printer x 2', 'Rockhampton', 'ROK 28/2.16','Cab3-S5', 'Multi Purpose', 'metal, 3D printing', null, 'https://www.sinterit.com/sinterit-lisa-pro/', 1),
-  (21, 'Wash and Sinter II', 'Rockhampton', 'ROK 28/2.16','Cab5-S2', 'Multi Purpose', 'metal', null, 'https://markforged.com/3d-printers/metal-x', 1),
-  (22, 'GoPro Hero 5 Black', 'Rockhampton', 'ROK 28/2.16','Cab1-S1', 'Multi Purpose', 'video', null, 'Standard lab induction', 1),
-  (23, 'Sony RX100 Camera', 'Rockhampton', 'ROK 28/2.13','Cab6-S5', 'Multi Purpose', 'photography, video', null, 'https://www.sony.com.au/electronics/cyber-shot-compact-cameras/dsc-rx100', 1),
-  (24, 'Desiccant Wheel', 'Bundaberg', 'G.15','Cab1-S4', 'Mechanical', 'rams project', null, null, 1),
-  (25, 'Lab Volt MV Test equipment', 'Bundaberg', 'G.15','Cab2-S2', '', 'power', null, null, 1),
-  (26, 'ETS7000 electronics breadboard and PS', 'Bundaberg', 'G.15','Cab3-S1', '', 'power', null, null, 1),
-  (27, 'Power Supplies', 'Bundaberg', 'G.15','Cab2-S5', 'Electrical', 'power, conversion', null, null, 1),
-  (28, 'Oscilloscopes', 'Bundaberg', 'G.15','Cab5-S2', 'Electrical', 'GUI, electrical signal', null, null, 1),
-  (29, 'Oscillators', 'Bundaberg', 'G.15','Cab6-S3', 'Civil', 'power, current conversion', null, null, 1),
-  (30, 'Multimeters', 'Bundaberg', 'G.15','Cab1-S5', 'Civil', 'power, measurements', null, null, 1),
-  (31, 'Basic hydraulic test Bench', 'Melbourne', 'Room 3.22','Cab2-S4', 'Mechanical', 'hydraulics', null, null, 1),
-  (32, 'Up box 3D printer', 'Melbourne', 'Room 3.22','Cab2-S6', 'Mechanical', '3D printing', null, null, 1),
-  (33, 'UP box 3D printer mini', 'Melbourne', 'Room 3.22','Cab3-S1', 'Mechanical', '3D printing', null, null, 1),
-  (34, 'Mitre saw', 'Melbourne', 'Room 3.22','Cab3-S4', 'Mechanical', 'sawing, angles', null, null, 1),
-  (35, 'portable ladder', 'Melbourne', 'Room 3.22','Cab2-S8', 'Mechanical', 'accessibility', null, null, 1)
+insert into equipment_items (equipment_name, equipment_campus, equipment_lab, equipment_location, equipment_discipline,
+equipment_details, equipment_media, equipment_whs, equipment_status) 
+values
+  ('Automatic Level', 'Cairns', 'ROK 28/LG.01', 'Cab1-S4', 'Civil', 'leveling', null, null, 1),
+  ('Automatic Level tripod', 'Cairns', 'ROK 28/LG.01', 'Cab1-S2', 'Civil', 'leveling', null, null, 1),
+  ('Leica Total Station (S06 plus)', 'Cairns', 'ROK 28/LG.04', 'Cab1-S3', 'Civil', 'surveying, measurements', null, null, 1),
+  ('Leica Total Station (S06)', 'Cairns', 'ROK 28/LG.04', 'Cab1-S4', 'Civil', 'surveying, measurements', null, null, 1),
+  ('leica total Station tripod', 'Cairns', 'ROK 28/LG.04', 'Cab2-S1', 'Civil', 'surveying, measurements', null, null, 1),
+  ('GNSS Leica Receiver (GS14)', 'Cairns', 'ROK 28/LG.04', 'Cab2-S3', 'Civil', 'communications', null, null, 1),
+  ('2 Tonne Bridge Crane', 'Cairns', 'ROK 28/LG.01', 'Cab3-S6', 'Civil', 'lifting', null, null, 1),
+  ('Flash Forge Creator 3 (FDM)', 'Melbourne', '6.25','Cab3-S3', 'Multi Purpose', '3D printing', null, 'https://www.flashforge.com/product-detail/1', 1),
+  ('Funmat HT 3D Printer (FDM)', 'Melbourne', '6.25','Cab1-S4', 'Multi Purpose', '3D printing', null, 'https://www.intamsys.com/funmat-ht-3d-printer/', 1),
+  ('Formbot Trex 3 (FDM)', 'Melbourne', '6.25','Cab4-S3', 'Multi Purpose', '3D printing', null, 'https://www.formbot3d.com/vivedino-t-rex-30-multi-function-big-size-3d-printer-p0062.html', 1),
+  ('Formlabs 2 (SLA)', 'Melbourne', '6.25','Cab1-S2', 'Multi Purpose', 'measurements', null, 'https://formlabs.com/3d-printers/form-2/', 1),
+  ('Tenma Programmable Power Supply 0-30V 10Amps', 'Melbourne', '6.25','Cab2-S4', '','remote control, power', null,'http://www.farnell.com/datasheets/2805374.pdf', 1),
+  ('FESTO / LabVolt 8006 Power & Machines Trainers (qty 3)', 'Melbourne', '6.25','Cab3-S5', 'Electrical', 'power', null,'https://www.labvolt.com/solutions/6_electricity_and_new_energy/59-8006-10_computer_assisted_0_2_kw_electromechanical_training_system', 1),
+  ('Oscilloscopes', 'Rockhampton', 'ROK 28/2.13','Cab1-S2', 'Multi Purpose', 'GUI, electrical signal', null, 'Standard lab induction', 1),
+  ('Oscillators', 'Rockhampton', 'ROK 28/2.13','Cab2-S1', 'Multi Purpose', 'power, current conversion', null, 'Standard lab induction', 1),
+  ('Multimeters', 'Rockhampton', 'ROK 28/2.13','Cab5-S4', 'Multi Purpose', 'power, measurements', null, 'Standard lab induction', 1),
+  ('Power Supplies', 'Rockhampton', 'ROK 28/2.13','Cab6-S3', 'Multi Purpose', 'power, conversion', null, 'Standard lab induction', 1),
+  ('Sinterit Lisa Pro 3D Printer', 'Rockhampton','ROK 28/2.16', 'Cab1-S4', 'Multi Purpose', '3D printing', null, 'Standard lab induction', 1),
+  ('Funmat HT 3D Printer', 'Rockhampton', 'ROK 28/2.16','Cab2-S2', 'Multi Purpose', 'FFF, 3D printing', null, 'Standard lab induction', 1),
+  ('Metal 3D Printer x 2', 'Rockhampton', 'ROK 28/2.16','Cab3-S5', 'Multi Purpose', 'metal, 3D printing', null, 'https://www.sinterit.com/sinterit-lisa-pro/', 1),
+  ('Wash and Sinter II', 'Rockhampton', 'ROK 28/2.16','Cab5-S2', 'Multi Purpose', 'metal', null, 'https://markforged.com/3d-printers/metal-x', 1),
+  ('GoPro Hero 5 Black', 'Rockhampton', 'ROK 28/2.16','Cab1-S1', 'Multi Purpose', 'video', null, 'Standard lab induction', 1),
+  ('Sony RX100 Camera', 'Rockhampton', 'ROK 28/2.13','Cab6-S5', 'Multi Purpose', 'photography, video', null, 'https://www.sony.com.au/electronics/cyber-shot-compact-cameras/dsc-rx100', 1),
+  ('Desiccant Wheel', 'Bundaberg', 'G.15','Cab1-S4', 'Mechanical', 'rams project', null, null, 1),
+  ('Lab Volt MV Test equipment', 'Bundaberg', 'G.15','Cab2-S2', '', 'power', null, null, 1),
+  ('ETS7000 electronics breadboard and PS', 'Bundaberg', 'G.15','Cab3-S1', '', 'power', null, null, 1),
+  ('Power Supplies', 'Bundaberg', 'G.15','Cab2-S5', 'Electrical', 'power, conversion', null, null, 1),
+  ('Oscilloscopes', 'Bundaberg', 'G.15','Cab5-S2', 'Electrical', 'GUI, electrical signal', null, null, 1),
+  ('Oscillators', 'Bundaberg', 'G.15','Cab6-S3', 'Civil', 'power, current conversion', null, null, 1),
+  ('Multimeters', 'Bundaberg', 'G.15','Cab1-S5', 'Civil', 'power, measurements', null, null, 1),
+  ('Basic hydraulic test Bench', 'Melbourne', 'Room 3.22','Cab2-S4', 'Mechanical', 'hydraulics', null, null, 1),
+  ('Up box 3D printer', 'Melbourne', 'Room 3.22','Cab2-S6', 'Mechanical', '3D printing', null, null, 1),
+  ('UP box 3D printer mini', 'Melbourne', 'Room 3.22','Cab3-S1', 'Mechanical', '3D printing', null, null, 1),
+  ('Mitre saw', 'Melbourne', 'Room 3.22','Cab3-S4', 'Mechanical', 'sawing, angles', null, null, 1),
+  ('portable ladder', 'Melbourne', 'Room 3.22','Cab2-S8', 'Mechanical', 'accessibility', null, null, 1)
 ;
 
 insert into staff values
@@ -114,12 +129,13 @@ insert into students values
   (12097543, 'Stan Johnston', 'stanjohnston@cqumail.com')
 ;
 
-insert into msds values
-  (1, 'Material Safety Data Sheets', 'https://www-msds-com-au.ezproxy.cqu.edu.au/DefaultMSDS.aspx?ReturnUrl=%2f'),
-  (2, 'First Aid', 'https://www.healthdirect.gov.au/first-aid'),
-  (3, 'Health Information Partners', 'https://www.healthdirect.gov.au/information-partners'),
-  (4, 'Emergency Services', 'https://info.australia.gov.au/information-and-services/public-safety-and-law/emergency-services'),
-  (5, 'Fire Safety', 'https://www.studyinaustralia.gov.au/english/live-in-australia/health-and-safety/fire')
+insert into msds (doc_name, doc_url)
+values
+  ('Material Safety Data Sheets', 'https://www-msds-com-au.ezproxy.cqu.edu.au/DefaultMSDS.aspx?ReturnUrl=%2f'),
+  ('First Aid', 'https://www.healthdirect.gov.au/first-aid'),
+  ('Health Information Partners', 'https://www.healthdirect.gov.au/information-partners'),
+  ('Emergency Services', 'https://info.australia.gov.au/information-and-services/public-safety-and-law/emergency-services'),
+  ('Fire Safety', 'https://www.studyinaustralia.gov.au/english/live-in-australia/health-and-safety/fire')
 ;
 
 INSERT INTO users(username, password, is_staff, name, contact, campus)
